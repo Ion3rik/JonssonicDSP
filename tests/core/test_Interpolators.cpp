9e89500850bade718db1d9b39b4d094a0d5da549
Jon Fagerström <jonkka@Jons-Air.home>
2025-11-18 12:50:49 +0200

    Implemented interpolator tests. All tests passed

---
+++ tests/core/test_Interpolators.cpp
@@ -0,0 +1,45 @@
+// Jonssonic - A C++ audio DSP library
+// Unit tests for Interpolators
+// Author: Jon Fagerström
+// Update: 18.11.2025
+
+#include <gtest/gtest.h>
+#include "Jonssonic/core/Interpolators.h"
+
+namespace Jonssonic::core {
+
+class InterpolatorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // common setup code
+    }
+
+    void TearDown() override {
+        // cleanup code
+    }
+
+    float buffer[4] = {0.0f, 10.0f, 20.0f, 30.0f};
+};
+
+// Tests for NearestInterpolator
+TEST_F(InterpolatorTest, NearestDown) {
+    float result = NearestInterpolator<float>::interpolate(buffer, 1, 0.3f);
+    EXPECT_FLOAT_EQ(result, 10.0f);
+}
+TEST_F(InterpolatorTest, NearestUp) {
+    float result = NearestInterpolator<float>::interpolate(buffer, 2, 0.7f);
+    EXPECT_FLOAT_EQ(result, 30.0f);
+}
+
+// Tests for LinearInterpolator
+TEST_F(InterpolatorTest, BasicInterpolation) {
+    float result = LinearInterpolator<float>::interpolate(buffer, 1, 0.5f);
+    EXPECT_NEAR(result, 15.0f, 1e-5);
+}
+
+TEST_F(InterpolatorTest, EdgeCase) {
+    float result = LinearInterpolator<float>::interpolate(buffer, 2, 0.0f);
+    EXPECT_FLOAT_EQ(result, 20.0f);
+}
+
+} // namespace Jonssonic::core
