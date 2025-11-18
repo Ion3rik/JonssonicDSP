512939676ae6e7e15f8b569be1b3db210025f1ae
Jon Fagerström <jonkka@Jons-Air.home>
2025-11-18 09:25:32 +0200

    Added initial file structure. Added DSPProcessor virtual class

---
+++ include/Jonssonic/core/DSPProcessor.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cstddef>
+
+namespace Jonssonic::Core
+{
+class DSPProcessor
+{
+public:
+    DSPProcessor() = default; // default constructor
+    virtual ~DSPProcessor() = default; // virtual destructor
+
+
+    // Prevent copying and moving
+    DSPProcessor(const DSPProcessor&) = delete; // no copy constructor
+    DSPProcessor& operator=(const DSPProcessor&) = delete; // no copy assignment
+    DSPProcessor(DSPProcessor&&) = delete; // no move constructor
+    DSPProcessor& operator=(DSPProcessor&&) = delete; // no move assignment
+
+
+    virtual void process(const float* const* input, float* const* output, size_t numOutChannels, size_t numInChannels, size_t numSamples) = 0;
+
+};
+} // namespace Jonssonic::Core
